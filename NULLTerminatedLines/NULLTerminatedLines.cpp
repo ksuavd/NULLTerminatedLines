3a614b363177c8b7312c5de16823bfc95fb48cea
ksuavd <eva2625@mail.ru>
2023-01-12 00:00:12 +0500

    Lesson #2

--- NULLTerminatedLines/NULLTerminatedLines.cpp
+++ NULLTerminatedLines/NULLTerminatedLines.cpp
@@ -4,9 +4,11 @@
 #include <windows.h>
 
 using namespace std;
-int  string_length (char str[]);
+int  string_length (const char str[]);
 void to_upper(char str[]);
+void to_lower(char str[]);
 void shrink(char str[]);
+bool is_palindrome(char str[]);
 int main()
 {
     setlocale(LC_ALL, "rus");
@@ -21,57 +23,86 @@ int main()
 */
     const int n = 200;
     char str[n] = {};
-    //const int z = 200;
-    //char str_a[z] = {};
     cout << "Введите строку: ";
     SetConsoleCP(1251);//переводим кодировку на русский для ввода
     //cin >> str;
     cin.getline(str, n);
     //  SetConsoleCP(866);//возвращем кодировку (не везде необходимо, в каких то версиях винд. автоматически возвращает и можно не прописывать)
-    cout << "Строка -" << str << "- состоит из " << string_length (str) << " символов." << endl;
+    cout << "Строка -" << str << "- состоит из " << strlen (str) << " символов." << endl;
     cout << "Строка -" << str << "- в верхнем регистре: "; to_upper(str); cout << str << endl;
-    cout << "Введите строку: "; cin.getline(str, n); cout<< "Удаляем лишние пробелы: " << endl;
+    cout << "Строка -" << str << "- в нижнем регистре: "; to_lower(str); cout << str << endl;
+    cout << "Введите строку: "; cin.getline(str, n); cout << "Удаляем лишние пробелы: "; shrink(str); cout << str << endl;
+    cout << "Строка " << (is_palindrome(str) ? "" : "НЕ ") << "палиндром!" << endl;
 }
 
-int  string_length(char str[])
+int  string_length(const char str[])
 {
-    int counter = 0; // счетчик символов
+    /* int counter = 0; // счетчик символов
     while (str[counter] != '\0')// перебираем массив когла доходим до ячйки которая хранит 0 останавливаем цикл увеличиваем счетчик
     {
         counter++;
     }
     return counter;
-
-    /*
-    for (int i;i< str[i] != 0;i++)
-    {
-    
-    }
-    */
+     */
+    int i = 0;
+    for (; str[i]; i++);
+    return i;
 }
 void to_upper(char str[])
 {
     for (int i = 0; str[i]; i++)
-    {
-        if (str[i] >= 'a' && str[i]<='z')
-        {
-            str[i] += 'A' - 'a';
-        }
-        if (str[i] >= 'а' && str[i] <= 'я')
+    {/*
+       if (str[i] >= 'а' && str[i] <= 'я')
         {
             str[i] += 'А' - 'а';
         }
+        if (str[i] >= 'a' && str[i] <= 'z')
+        {
+            str[i] += 'A' - 'a';
+        }
+        */
+        str[i] = toupper(str[i]);
     }
 }
-void shrink(char str[])
+void to_lower(char str[])
 {
-    int space = 0;
     for (int i = 0; str[i]; i++)
     {
-        if (str[i] == ' ')
+        str[i] =tolower (str[i]);
+    }
+}
+
+void shrink(char str[])
+{
+    for (int i = 0; str[i]; i++)
+    {
+        while(str[i] == ' '&&str[i+1]==' ')
         {
-            if (space == 0);
-            if (str[i + 1] == ' ');
+            for (int j=i+1; str[j]; j++)
+            {
+                str[j] = str[j + 1];
+            }
         }
     }
+}
+void remove_symbol(char str[], char symbol)
+{
+    for (int i = 0; str[i]; i++)
+    {
+        while (str[i]==symbol)
+        {
+            for (int j = i; str[j]; j++)str[j] = str[j + 1];
+        }
+    }
+}
+bool is_palindrome(char str[])
+{
+    to_lower(str);
+    remove_symbol(str, ' ');
+    int n = strlen(str);
+    for (int i = 0; i < n / 2; i++)
+    {
+        if (str[i] != str[n - 1 - i])return false;
+     }
+    return true;
 }
